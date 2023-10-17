90d3076ae0bd4e3fb5b85301c7363dc55f719052
harinee05 <harini.p98@gmail.com>
2023-10-16 23:00:49 -0400

    Created a page and added background color

---
+++ portfolio/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body className='App-body'>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Hey Gopa!!!!
+        </h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
